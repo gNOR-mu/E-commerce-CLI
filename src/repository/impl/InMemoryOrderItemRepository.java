6d84f9d75a2b7ba06608b3d305a37cfbc3ae0bbe
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-01-03 00:52:47 -0300

    Corregido el modelo del repositorio

--- src/repository/impl/InMemoryOrderItemRepository.java
+++ src/repository/impl/InMemoryOrderItemRepository.java
@@ -1,7 +1,7 @@
 package repository.impl;
 
-import model.Order;
-import repository.OrderRepository;
+import model.OrderItem;
+import repository.OrderItemRepository;
 
-public class InMemoryOrderItemRepository extends InMemoryAbstractRepository<Order, Long> implements OrderRepository {
+public class InMemoryOrderItemRepository extends InMemoryAbstractRepository<OrderItem, Long> implements OrderItemRepository {
 }
