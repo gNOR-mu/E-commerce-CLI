4e21148c55b12bc6d1bdc3124c08628126a28f14
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2026-01-03 16:46:15 -0300

    refactorización de menus

--- src/main/java/com/ecommerce/demo/Main.java
+++ src/main/java/com/ecommerce/demo/Main.java
@@ -1,8 +1,7 @@
 package com.ecommerce.demo;
 
-import com.ecommerce.demo.view.AdminMenu;
-import com.ecommerce.demo.view.MenuRunner;
-import com.ecommerce.demo.view.UserMenu;
+
+import com.ecommerce.demo.view.MainMenu;
 
 import java.util.Scanner;
 
@@ -11,36 +10,8 @@ public class Main {
     public static void main(String[] args) {
         DependencyContainer dependencyContainer = new DependencyContainer();
         Scanner scanner = new Scanner(System.in);
-        int option = -1;
-        boolean run = true;
-
-        while (run) {
-            showMenu();
-            try {
-                System.out.print("Ingrese una opción: ");
-                option = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("Opción inválida");
-                scanner.nextLine();
-            }
-            switch (option) {
-                case 0 -> run = false;
-                case 1 -> MenuRunner.run(new AdminMenu(scanner));
-                case 2 -> MenuRunner.run(new UserMenu(scanner));
-                default -> System.out.println("Opción inválida");
-            }
-
-
-        }
-
+        MainMenu mainMenu = new MainMenu(dependencyContainer, scanner);
+        mainMenu.show();
         scanner.close();
-    }
-
-    public static void showMenu() {
-        System.out.println("*** Menú principal ***\n" +
-                "1) ADMIN\n" +
-                "2) USUARIO\n" +
-                "0) Salir\n"
-        );
     }
 }
--- src/main/java/com/ecommerce/demo/view/MainMenu.java
+++ src/main/java/com/ecommerce/demo/view/MainMenu.java
@@ -1,23 +1,26 @@
 package com.ecommerce.demo.view;
 
+import com.ecommerce.demo.DependencyContainer;
+
 import java.util.Scanner;
 
-public class AdminMenu extends AbstractMenu {
-    //TODO Definir servicios a utilizar
+public class MainMenu extends AbstractMenu {
+    private final DependencyContainer dependencyContainer;
 
-    public AdminMenu(Scanner scanner) {
+    public MainMenu(DependencyContainer dependencyContainer, Scanner scanner) {
         super(scanner);
+        this.dependencyContainer = dependencyContainer;
     }
 
     @Override
     protected void printMenuOptions() {
         System.out.println("""
                 **********************
-                *** Menú ADMIN ***
+                *** Menú principal ***
                 **********************
                 
-                1) -
-                2) -
+                1) ADMIN
+                2) USUARIO
                 0) SALIR
                 """);
     }
@@ -28,6 +31,7 @@ public class AdminMenu extends AbstractMenu {
             case 0 -> {
                 return false;
             }
+            case 1 -> MenuRunner.run(new AdminMenu(scanner));
             default -> System.out.println("Opción inválida");
         }
         return true;
