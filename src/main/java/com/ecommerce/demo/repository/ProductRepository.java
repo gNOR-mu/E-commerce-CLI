8b9c0fd7e11275a872a5df826ac2886d0f58ef62
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2026-01-03 13:09:35 -0300

    busqueda por nombre y categoría añadido

--- src/main/java/com/ecommerce/demo/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/demo/repository/ProductRepository.java
@@ -2,6 +2,24 @@ package com.ecommerce.demo.repository;
 
 import com.ecommerce.demo.model.Product;
 
+import java.util.List;
+
+/**
+ * Define el contrato de acceso a datos para la entidad {@link Product}.
+ * <p>
+ * Extiende las operaciones CRUD básicas y añade consultas específicas
+ * relacionadas con el dominio de los productos.
+ * </p>
+ *
+ * @author Gabriel Norambuena
+ * @version 1.0
+ * @see CrudRepository
+ */
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
+    //TODO JAVADOC
+    List<Product> findByNameIgnoreCase(String name);
+
+    //TODO JAVADOC
+    List<Product> findByCategoryId(Long categoryId);
 }
--- src/main/java/com/ecommerce/demo/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/ecommerce/demo/repository/impl/InMemoryProductRepository.java
@@ -3,5 +3,44 @@ package com.ecommerce.demo.repository.impl;
 import com.ecommerce.demo.model.Product;
 import com.ecommerce.demo.repository.ProductRepository;
 
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Implementación en memoria del repositorio de productos.
+ * <p>
+ * Utiliza un {@link java.util.HashMap} para simular la persistencia de datos.
+ * Esta implementación no es persistente: los datos se perderán al reiniciar la aplicación.
+ * </p>
+ *
+ */
 public class InMemoryProductRepository extends InMemoryAbstractRepository<Product> implements ProductRepository {
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * <b>Nota de implementación:</b> Realiza un filtrado secuencial sobre todos los
+     * elementos en memoria.
+     * </p>
+     */
+    @Override
+    public List<Product> findByNameIgnoreCase(String name) {
+        return DB.values().stream()
+                .filter(product -> product.getName().equalsIgnoreCase(name))
+                .toList();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * <b>Nota de implementación:</b> Realiza un filtrado secuencial sobre todos los
+     * elementos en memoria.
+     * </p>
+     */
+    @Override
+    public List<Product> findByCategoryId(Long categoryId) {
+        return DB.values().stream()
+                .filter(product -> product.getCategoryId().equals(categoryId))
+                .toList();
+    }
 }
